10645ce45465105e7b66b51544f037b4aecd8b75
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 22:29:45 -0400

    parsing done

--- pubchem.js
+++ pubchem.js
@@ -1,60 +1,52 @@
-async function init() {
-
-  const localcompounds = [];
-const n = 10;
-for (let i = 1; i < n; i++){
-  fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/${i}/JSON/`)
-    .then(response => response.json())
-    .then(data => localcompounds.push(data))
-    await sleep(1000);
+const requestCompounds = async (n, delay) => {
+  try {
+    const compounds = [];
+    for (let i = 1; i <= n; i++) {
+      const url = `https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/${i}/JSON/`;
+      const res = (await fetch(url));
+      console.log(res);
+      compounds.push(res.data);
+      await new Promise(r => setTimeout(r, delay));
+    }
+    return compounds;
+  } catch(e) {
+    console.log(`An error occurred while fetching compound ${n}`);
+    console.log(e);
   }
-  return localcompounds;
 }
 
-function sleep(ms) {
-  return new Promise((resolve)=> {
-      setTimeout(resolve, ms);
+const parseCompounds = compounds => {
+  return compounds.map(({Record}) => {
+    const {Section, RecordNumber, RecordTitle} = Record;
+
+    const getSubsectionInfo = (baseObj, propertyName) => baseObj?.find(({TOCHeading}) => TOCHeading == propertyName)?.Information;
+    const getDeepValue = obj => obj?.[0]?.Value?.StringWithMarkup?.[0]?.String;
+
+    const namesAndIds = Section?.find(({TOCHeading}) => TOCHeading == "Names and Identifiers")?.Section;
+    const computedDescriptors = namesAndIds?.find(({TOCHeading}) => TOCHeading == "Computed Descriptors")?.Section;
+
+    const recordDescription = namesAndIds?.find(({TOCHeading}) => TOCHeading == "Record Description")?.Information?.find(({Name}) => Name == "Record Description")?.Value?.StringWithMarkup?.[0]?.String;
+
+    return {
+      'number' : RecordNumber,
+      'name' : RecordTitle,
+      'description' : recordDescription,
+      'iupac' : getDeepValue(getSubsectionInfo(computedDescriptors, "IUPAC Name")),
+      'inchi' : getDeepValue(getSubsectionInfo(computedDescriptors, "InChI")),
+      'inchikey' : getDeepValue(getSubsectionInfo(computedDescriptors, "InChI Key")),
+      'smiles' : getDeepValue(getSubsectionInfo(computedDescriptors, "Canonical SMILES")),
+    };
   })
 }
 
-async function process() {
-  const mastercompounds = await init()
-  const newcompounds = []
-  mastercompounds.forEach(compound => {
-    const firstSection = compound.Record.Section;
-    const sectionIndex = firstSection.findIndex(({TOCHeading}) => TOCHeading == "Names and Identifiers");
-
-    const secondSection = firstSection[sectionIndex]["Section"];
-
-    const nextSectionIndex = secondSection.findIndex(section => section["TOCHeading"] == "Record Description");
-    
-    const computedDescriptorsIndex = secondSection.findIndex(section => section["TOCHeading"] == "Computed Descriptors");
-    const descriptionSection = secondSection[computedDescriptorsIndex]["Section"];
-
-    const iupacIndex = descriptionSection.find(section => section["TOCHeading"] == "IUPAC Name");
-    const inchiIndex = descriptionSection.find(section => section["TOCHeading"] == "InChI");
-    const inchiKeyIndex = descriptionSection.find(section => section["TOCHeading"] == "InChI Key");
-    const smilesIndex = descriptionSection.find(section => section["TOCHeading"] == "Canonical SMILES");
-
-    const root = compound.Record.Section[sectionIndex];
-    const getStringWithMarkup = obj => obj?.Information?.[0]?.Value?.StringWithMarkup?.[0]?.String;
-    const getDescriptor = obj => obj?.root?.[computedDescriptorsIndex]?.["Section"];
-    newcompounds.push({
-      'number' : compound.Record.RecordNumber,
-      'name' : compound.Record.RecordTitle,
-      'description' : getStringWithMarkup(root[nextSectionIndex]),
-      'iupac' : getStringWithMarkup(getDescriptor(iupacIndex)),
-      'inchi' : getStringWithMarkup(getDescriptor(inchiIndex)),
-      'inchikey' : getStringWithMarkup(getDescriptor(inchiKeyIndex)),
-      'smiles' : getStringWithMarkup((smilesIndex))
-    });
-
-    // console.log(newcompounds.length);
-  })
-  
-  console.log(newcompounds);
-  console.log(newcompounds.length);
-
+const main = async () => {
+  const delay = 1000;
+  const numCompounds = 5;
+  const compounds = await requestCompounds(numCompounds, delay);
+  console.log(compounds);
+  // const parsed = parseCompounds(compounds);
+  // console.log(parsed);
+  return;
 };
 
-process();
+main();
